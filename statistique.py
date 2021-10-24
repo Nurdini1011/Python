b584d2d329ca39552acd23a626b694fd9ac91542
Nurdini1011 <91740786+Nurdini1011@users.noreply.github.com>
2021-10-24 22:21:41 +0200

    Add files via upload

--- statistique.py
+++ statistique.py
@@ -3,7 +3,39 @@ import re
 import json
 from tkinter import *
 import os
-from log_json import cherche_heure,cherche_OS
+
+def cherche_OS(liste): 
+    #créé la liste pour compter les OS utilisé
+    data={'Windows':0,'Mac OS X':0,'iOS 6_0':0,'Linux':0,'Unknown':0}
+    
+    #on traverse dans la tableau pour chercher le system_agent utilisé et le comparer
+    for i in liste:
+        if i['system_agent']=='Windows' :
+            #data[0] est pour compter le system_agent de Windows
+            data['Windows']=data['Windows']+1
+        if i['system_agent']=='Mac OS X' :
+            #data[1] est pour compter le system_agent de Mac OS X
+            data['Mac OS X']=data['Mac OS X']+1
+        if i['system_agent']=='iOS 6_0':
+            #data[2] est pour compter le system_agent de iOS 6_0X
+            data['iOS 6_0']=data['iOS 6_0']+1
+        if i['system_agent']=='Linux':
+            #data[3] est pour compter le system_agent de Linux
+            data['Linux']=data['Linux']+1
+        if i['system_agent']=='Unknown':
+            data['Unknown']=data['Unknown']+1
+    return data
+
+def cherche_heure(dictionaire):
+    heure={'00':0,'01':0,'02':0,'03':0,'04':0,'05':0,'06':0,'07':0,'08':0,'09':0,'10':0,'11':0,'12':0,'13':0,'14':0,'15':0,'16':0,'17':0,'18':0,'19':0,'20':0,'21':0,'22':0,'23':0} 
+    j=0
+    for i in dictionaire: 
+       j=j+1 
+       for hour in heure: 
+           tab=i['time'].split(':') 
+           if(tab[1]==hour): 
+              heure[hour]=heure[hour]+1 
+    return heure
 
 #les statistiques	
 def statistique_all(f):
